0714e9e0ef324c6c439444eb2137aeb22033e21d
davenbezz <50683672+davenbezz@users.noreply.github.com>
2021-01-30 00:39:38 +0300

    I've finished the dom

--- assets/js/bom.js
+++ assets/js/bom.js
@@ -15,16 +15,33 @@ Good Luck !!!
 */
 
 
-
-
 // Define UI Variables  here 
+const collection1 = document.querySelector('#collection1');
+const collection2 = document.querySelector('#collection2');
+const collection3 = document.querySelector('#collection3');
+const collection4 = document.querySelector('#collection4');
 
+const items1 = collection1.getElementsByTagName('span');
+const items2 = collection2.getElementsByTagName('span');
+const items3 = collection3.getElementsByTagName('span');
+const items4 = collection4.getElementsByTagName('span');
 
-
-
-
-
-
+const locationInformation = [location.href, location.protocol, location.host, location.port, location.hostname];
+const browserInformation = [navigator.appName, navigator.appVersion, navigator.platform, navigator.language, navigator.cookieEnabled];
+const screenInformation = [screen.height, screen.width, screen.pixelDepth];
+const historyInformation = [history.length, history.state];
 
 
 // Display the BOM Information on the innerHTML of the elements
+for(let i = 0; i < items1.length; i++){
+    items1[i].innerHTML = locationInformation[i];
+}
+for(let i = 0; i < items2.length; i++){
+    items2[i].innerHTML = browserInformation[i];
+}
+for(let i = 0; i < items3.length; i++){
+    items3[i].innerHTML = screenInformation[i];
+}
+for(let i = 0; i < items4.length; i++){
+    items4[i].innerHTML = historyInformation[i];
+}
